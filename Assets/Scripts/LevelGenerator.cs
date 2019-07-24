5d75fa8b318e2e8e8e659d831c313c6a27061447
angelsophia1 <angelsunyulei@126.com>
2019-07-24 12:25:45 +0800

    Initial commit

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+    public GameObject platformPrefab;
+
+    public int numberOfPlatforms;
+    public float levelWidth = 6f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 spawnPosition = new Vector3(0f,-2.3f,0f);
+        for (int i = 0; i < numberOfPlatforms;i++ )
+        {
+            spawnPosition.y += Random.Range(minY,maxY);
+            spawnPosition.x = Random.Range(-levelWidth,levelWidth);
+            Instantiate(platformPrefab,spawnPosition,Quaternion.identity);
+        }
+	}
+	
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+    private float movement = 0f;
+    public float movementSpeed = 10f;
+    public GameObject restartButton,loseComparisonPosition,leftBoudary,RightBoundary;
+    Rigidbody2D rb;
+    
+    // Use this for initialization
+	void Start () {
+        Time.timeScale = 1f;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+         movement = Input.GetAxis("Horizontal");
+        if (transform.position.y < loseComparisonPosition.transform.position.y)
+        {
+            Time.timeScale = 0;
+            restartButton.SetActive(true);
+        }
+        if (transform.position.x < leftBoudary.transform.position.x)
+        {
+            transform.position = new Vector3(-transform.position.x,transform.position.y,transform.position.z);
+        }else if (transform.position.x>RightBoundary.transform.position.x)
+        {
+            transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement * movementSpeed;
+        rb.velocity = velocity;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("My Jumper");
+    }
+}
